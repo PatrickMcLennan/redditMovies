60a6259a2ab43e1504cbf57ca765309473a5b15b
Patrick <patrick.a.mclennan@gmail.com>
2019-04-26 13:25:04 -0400

    setting up front end

---
+++ client/components/App/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  render(): JSX.Element {
+    return (
+      <div>
+        <h1>hello</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
