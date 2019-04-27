ef515ff3a335ce9c6bce83ca7e9f4bdb2a05df11
Patrick <patrick.a.mclennan@gmail.com>
2019-04-27 09:54:44 -0400

    setting up front end

---
+++ client/components/Nav/__tests__/Nav.test.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { cleanup, render } from 'react-testing-library';
+import 'jest-styled-components';
+import 'jest-dom/extend-expect';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../../../utils/globalStyles';
+import Nav from '../Nav';
+
+afterEach(cleanup);
+
+const renderNav = () =>
+  render(
+    <ThemeProvider theme={theme}>
+      <Nav />
+    </ThemeProvider>
+  );
+
+test('<Nav showMenu="false" />', () => {
+  const { getByTestId } = renderNav();
+  const nav = getByTestId('nav');
+  const logo = getByTestId('logo');
+
+  // Content
+  expect(nav).toContainElement(logo);
+  // Styles
+  expect(nav).toHaveStyleRule('text-align', 'center');
+});
